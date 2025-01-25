1416f72431443088665f6dcaf23d2c78e6550d50
Alameen Sabbah <70842282+ZeroNeroIV@users.noreply.github.com>
2025-01-25 17:33:52 +0300

    Fixed misunderstanding of last task

--- Components/Task24_MyFunctionPage.jsx
+++ Components/Task24_MyFunctionPage.jsx
@@ -1,6 +1,9 @@
+import { useImperativeHandle, useState } from "react";
 import { Text } from "react-native";
 
 export function Task24_MyFunctionPage(props) {
+    const [text, setText] = useState("Task24")
+
     const styles = {
         textStyle: {
             fontSize: 20,
@@ -9,9 +12,19 @@ export function Task24_MyFunctionPage(props) {
         }
     }
 
+    function imperative_handler() {
+        return ({
+            changeText(str) {
+                setText(str);
+            },
+        });
+    }
+
+    useImperativeHandle(props.text, imperative_handler);
+
     return (
         <Text style={styles.textStyle}>
-            {props.text}
+            {text}
         </Text>
     )
 }
--- Tasks/Task24.jsx
+++ Tasks/Task24.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { View, TextInput } from "react-native";
 import { Task24_MyFunctionPage } from "../Components/Task24_MyFunctionPage";
 
 export function Task24() {
-    const [text, setText] = useState("Task24");
+    const textRef = useRef();
 
     function on_text_input_change(new_text) {
-        setText(new_text);
+        textRef.current.changeText(new_text);
     }
 
     return (
         <View>
-            <Task24_MyFunctionPage text={text} />
+            <Task24_MyFunctionPage text={textRef} />
             <TextInput onChangeText={on_text_input_change}>
             </TextInput>
         </View>
